86e7a186bb860c4837cde77501e2286dfa9f09e3
geone96 <geone.lemobs@gmail.com>
2021-01-28 00:53:53 -0300

    removendo log de erros 400

--- src/utils/validation/validation-exception.factory.ts
+++ src/utils/validation/validation-exception.factory.ts
@@ -2,6 +2,5 @@ import { ValidationError } from 'class-validator';
 import { BadRequestException } from '@nestjs/common';
 
 export default function(errors: ValidationError[]): BadRequestException {
-    console.log(errors);
     return new BadRequestException(errors.map(err => Object.values(err.constraints)).join(', '));
 }
